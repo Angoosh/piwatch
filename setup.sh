c514838ca0821daac5150261adde653629abba65
Angoosh <kaplanantonin@gmail.com>
2018-04-18 21:30:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@
 apt install python-pip zlib1g-dev libjpeg-dev
 pip install pyowm
 pip install git+https://github.com/rm-hull/luma.oled
+rm /usr/local/share/fonts
+mkdir /usr/local/share/fonts
 mv /home/pi/watch/FreeMono.ttf /usr/local/share/fonts
