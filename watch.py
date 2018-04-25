5cf3c8b0c7c4e5b4058e4b100cc0993ca2f43c15
Angoosh <kaplanantonin@gmail.com>
2018-04-25 23:59:33 +0200

    Update watch.py

--- watch.py
+++ watch.py
@@ -5,9 +5,7 @@ from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-import pyowm
 import os
-import urllib2
 import RPi.GPIO as GPIO
 import psutil
 
@@ -16,10 +14,10 @@ s = 0
 
 # Inderrupts
 def menuadd(channel):
-        global s
+	global s
         s+=1
 def menusub(channel):
-        global s
+	global s
         s-=1
 
 
@@ -34,15 +32,6 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(21, GPIO.FALLING, callback=menuadd, bouncetime=300)
 GPIO.add_event_detect(20, GPIO.FALLING, callback=menusub, bouncetime=300)
 
-# Network
-def network():
-        try:
-                urllib2.urlopen('http://216.58.192.142', timeout=1)
-                return True
-        except urllib2.URLError as err:
-                return False
-
-a = network()
 # LCD
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial, rotate=0)
@@ -53,75 +42,55 @@ fond = ImageFont.truetype("/usr/local/share/fonts/FreeMono.ttf", 40, encoding="u
 
 # Weather station
 place = 'Pardubice'
-owm = pyowm.OWM('4612172c6662b19a70dc25af195666a0')
 
 # Printing on the LCD
-if network() and s == 1:
-        observation = owm.weather_at_place(place+',CZ')
-        w = observation.get_weather()
-        t = str(w.get_temperature(unit='celsius'))
+
 def second():
-        if network() and s == 1:
-                observation = owm.weather_at_place(place+',CZ')
-                w = observation.get_weather()
-                t = str(w.get_temperature(unit='celsius'))
-                list = t.split(',')
-                lst = list[2].split(': ')
-        with canvas(device) as draw:
-                draw.text((0, 0), strftime("%H:%M"), font = font, fill = "white" )
-                draw.text((63, 0), strftime(".%S"), font = fon, fill="white")
-                draw.text((60, 19), strftime("%d-%m-%y"), font = fon, fill = "white" )
-                if network():
-                        draw.text((0, 45), lst[1]+"C", font = font, fill = "white" )
-                        draw.text((0, 32), place, font = fon, fill = "white" )
-                else:
-                        draw.text((0, 45), "network err", font=fon, fill="white")
+	with canvas(device) as draw:
+		draw.text((0, 0), strftime("%H:%M"), font = font, fill = "white" )
+		draw.text((63, 0), strftime(".%S"), font = fon, fill="white")
+		draw.text((60, 19), strftime("%d-%m-%y"), font = fon, fill = "white" )
+		file = open("weather.info","r")
+		dataread = file.read()
+		if dataread!="NULL" or "":
+			draw.text((0, 45), dataread+"C", font = font, fill = "white" )
+			draw.text((0, 32), place, font = fon, fill = "white" )
+		else:
+			draw.text((0, 45), "network err", font=fon, fill="white")
 
 def first():
-        if network() and s == 1:
-                observation = owm.weather_at_place(place+',CZ')
-                w = observation.get_weather()
-                t = str(w.get_temperature(unit='celsius'))
-                list = t.split(',')
-                lst = list[2].split(': ')
-        with canvas(device) as draw:
-                draw.text((0, 0), strftime("%H:%M"), font = fond, fill="white")
+      with canvas(device) as draw:
+		draw.text((0, 0), strftime("%H:%M"), font = fond, fill="white")
                 draw.text((0, 40), strftime("%d-%m-%y"), font = fon, fill="white")
-                if GPIO.input(26)==0:
-                        while GPIO.input(16)==1:
-                                device.clear()
+		if GPIO.input(26)==0:
+			while GPIO.input(16)==1:
+				device.clear()
 
 def third():
-        if network() and s == 1:
-                observation = owm.weather_at_place(place+',CZ')
-                w = observation.get_weather()
-                t = str(w.get_temperature(unit='celsius'))
-                list = t.split(',')
-                lst = list[2].split(': ')
         with canvas(device) as draw:
-                draw.text((105, 47), "PID:", font=pid, fill="white")
+		draw.text((105, 47), "PID:", font=pid, fill="white")
                 draw.text((105, 55), str(os.getpid()), font=pid, fill="white")
-                draw.text((0, 0), "CPU:"+str(psutil.cpu_percent())+"%", font=font, fill="white")
-                draw.text((0, 30), "shutdown:", font=fon, fill="white")
-                draw.text((0, 40), "reboot:", font=fon, fill="white")
-                if GPIO.input(16)==0:
-                        device.clear()
-                        os.system("sudo shutdown now")
-                elif GPIO.input(26)==0:
-                        device.clear()
-                        os.system("sudo reboot")
+		draw.text((0, 0), "CPU:"+str(psutil.cpu_percent())+"%", font=font, fill="white")
+		draw.text((0, 30), "shutdown:", font=fon, fill="white")
+		draw.text((0, 40), "reboot:", font=fon, fill="white")
+		if GPIO.input(16)==0:
+			device.clear()
+			os.system("sudo shutdown now")
+		elif GPIO.input(26)==0:
+			device.clear()
+			os.system("sudo reboot")
 while True:
-        try:
-                if s==0:
-                        first()
-                elif s==1:
-                        second()
-                elif s==2:
-                        third()
+	try:
+		if s==0:
+			first()
+		elif s==1:
+			second()
+		elif s==2:
+			third()
 
-                if s<0:
-                        s = 0
-                if s>2:
-                        s = 2
-        except:
-                pass
+		if s<0:
+			s = 0
+		if s>2:
+			s = 2
+	except:
+		pass
