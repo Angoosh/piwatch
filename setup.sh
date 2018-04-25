677517d72221577081f3aee2a7897d356c00b4e4
Angoosh <kaplanantonin@gmail.com>
2018-04-26 00:02:42 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt install python-pip zlib1g-dev libjpeg-dev
-pip install pyowm
-pip install psutil
-pip install git+https://github.com/rm-hull/luma.oled
-rm /usr/local/share/fonts
-mkdir /usr/local/share/fonts
-mv /home/pi/watch/FreeMono.ttf /usr/local/share/fonts
