899f0b91a60d9874218ac184f3a8300ff6c3a123
Angoosh <kaplanantonin@gmail.com>
2018-04-26 00:45:57 +0200

    Update init.py

--- init.py
+++ init.py
@@ -4,11 +4,5 @@ from time import sleep
 
 
 sleep(2)
-#os.system("sh /home/pi/watch/start.sh")
-os.system("cd /home/pi/watch")
-os.system("python weather.py &")
-print("Weather process started")
-sleep(3)
-os.system("python watch.py &")
-os.system("cd /home/pi")
-print("Watch process started")ext
+os.system("sh /home/pi/watch/start.sh")
+
